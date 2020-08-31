908d8ff02416263ee51e903b8bd16c66e6fa8737
MikhailRommulo <mikhailrommulo@gmail.com>
2020-08-31 15:44:54 -0300

    version api

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      version: '1.0.0',
+      description: 'CRUD com NestJS e NestJSX'
+    };
   }
 }
